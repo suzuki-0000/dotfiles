41ed5035da2b46111be61df09731ff084177da13
keishi suzuki <hall_statt@mylife.(none)>
2013-06-18 01:22:44 +0900

    sh commit

---
+++ read.sh
@@ -0,0 +1,3 @@
+echo 'write to platform...'
+cp -r ~/dotfiles/.vim ~/dotfiles/.vimrc ~/
+echo 'write to platform... has done.'
---
+++ write.sh
@@ -0,0 +1,3 @@
+echo 'goes to for git...'
+cp -r ~/.vim ~/.vimrc ~/dotfiles/
+echo 'goes to for git... has done.'
