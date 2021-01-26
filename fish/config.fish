aba08ccbe9a1105c75ca950422cb07e0d11ec4fc
keishi_suzuki <keishi.1983@gmail.com>
2021-01-26 14:31:05 +0900

    update fish config.

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,16 @@
-export CHANGELOG_GITHUB_TOKEN="c3778b3f0c2f91b66887b92ae5fc07dbcf483da6"
+export CHANGELOG_GITHUB_TOKEN="c9862a1416f89f9d9ff3eac1da58468125f61bf2"
 export YAHOO_EMAIL="kc_suzuki_1983"
 export YAHOO_PASSWORD="steelbird996"
-export XCODE_XCCONFIG_FILE="/Users/nml_dev/ghq/cathage-helper.xcconfig"
-
 set -x PATH /usr/local/opt/openssl/bin $PATH
-set -x PATH $HOME/.rbenv/bin $PATH
-set -x PATH $HOME/.pyenv/shims $PATH
-set -x PATH $HOME/.nodebrew/current/bin $PATH
+set -x PATH ~/.rbenv/bin $PATH
 set -x LDFLAGS "-L/usr/local/opt/zlib/lib"
 set -x CPPFLAGS "-I/usr/local/opt/zlib/include"
 
-#set -x PATH $HOME/.pyenv/bin $PATH
-#. (pyenv init - | psub)
+rbenv init - | source
+
+set -x PATH $HOME/.pyenv/bin $PATH
+. (pyenv init - | psub)
+
 
 ###########
 # basic
@@ -36,11 +35,16 @@ end
 # git 
 ###########
 alias gs="git status"
-alias gd="git diff"
 alias gb="git branch"
-alias gl='git log --graph --all --format="%x09%C(cyan bold)%an%Creset%x09%C(yellow)%h%Creset %C(magenta reverse)%d%Creset %s"'
+alias gbd="git branch -D"
+alias gc="git checkout"
+alias ga="git add ."
+alias gca="git commit -a"
+alias gl='git log --graph --oneline --decorate --all'
+alias glp='git log --pretty=format:"%h %ad %s" --date=short --all'
 
 ###########
 # other 
 ###########
+alias fish="vim ~/.config/fish/config.fish"
 alias vimrc="vim ~/.vimrc"
