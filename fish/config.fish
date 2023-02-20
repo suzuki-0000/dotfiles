6bbb58dff09065f859a80b37ae62c66c814ea0e4
suzuki-0000 <keishi.1983@gmail.com>
2023-02-20 16:40:30 +0900

    add config fish.

--- fish/config.fish
+++ fish/config.fish
@@ -1,46 +1,61 @@
+export CODECOV_TOKEN=0eeedf54-576c-48db-99d0-83b0d2bdf77e
 export CHANGELOG_GITHUB_TOKEN="c3778b3f0c2f91b66887b92ae5fc07dbcf483da6"
-export YAHOO_EMAIL="kc_suzuki_1983"
-export YAHOO_PASSWORD="steelbird996"
+export OPENAI_API_KEY="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" 
+
 export XCODE_XCCONFIG_FILE="/Users/nml_dev/ghq/cathage-helper.xcconfig"
+export NVM_DIR="$HOME/.nvm"
+export PATH="$HOME/.nodebrew/current/bin:$PATH"
 
+#set PATH /opt/homebrew/bin $PATH
+set -x PATH /opt/homebrew/bin $PATH
 set -x PATH /usr/local/opt/openssl/bin $PATH
-set -x PATH $HOME/.rbenv/bin $PATH
-set -x PATH $HOME/.pyenv/shims $PATH
+
 set -x PATH $HOME/.nodebrew/current/bin $PATH
-set -x LDFLAGS "-L/usr/local/opt/zlib/lib"
-set -x CPPFLAGS "-I/usr/local/opt/zlib/include"
+set -x PATH $HOME/.rbenv/bin $PATH
+status --is-interactive; and source (rbenv init -|psub)
 
-#set -x PATH $HOME/.pyenv/bin $PATH
-#. (pyenv init - | psub)
+### basic - fish ###
+alias fi='vim ~/.config/fish/config.fish'
+alias fr='cd | . ~/.config/fish/config.fish'
 
-###########
-# basic
-###########
-alias ps="ps aux"
-alias ll="ls -aio"
+### basic - primitive ###
+alias ll='lsd -la'
 alias cp="cp -p"
 alias rm="rm -i"
 alias mv="mv -i"
-alias g='cd (ghq root)/(ghq list | peco)'
-alias gh='hub browse (ghq list | peco | cut -d "/" -f 2,3)'
+alias ps="ps aux"
+alias dps="docker compose ps"
 alias hosts="sudo vi /etc/hosts"
+alias teee="tee >(pbcopy)"
 
-###########
-# history
-###########
+### basic - git ###
+alias gc='git checkout'
+alias gcp='git branch | peco | xargs git checkout'
+alias gs="git status"
+alias gf="git branch -a --sort=-authordate | grep -v -e '->' -e '*' | perl -pe 's/^\h+//g' | perl -pe 's#^remotes/origin/##' |  peco | xargs git fetch origin"
+
+alias gcm='git commit -m'
+alias gb="git branch"
+alias gca="git commit --amend"
+alias gD="git branch --merged|egrep -v '\*|main'|xargs git branch -d"
+alias gd='git branch | peco | xargs git branch -D'
+
+alias gl='git log --graph --all --format="%x09%C(cyan bold)%an%Creset%x09%C(yellow)%h%Creset %C(magenta reverse)%d%Creset %s"'
+alias see='gh repo view --web'
+alias seep='gh pr view --web'
+
+### peco - history ###
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
 end
 
-###########
-# git 
-###########
-alias gs="git status"
-alias gd="git diff"
-alias gb="git branch"
-alias gl='git log --graph --all --format="%x09%C(cyan bold)%an%Creset%x09%C(yellow)%h%Creset %C(magenta reverse)%d%Creset %s"'
+### peco - ghq ###
+alias g='cd (ghq root)/(ghq list | peco)'
+
 
 ###########
 # other 
 ###########
 alias vimrc="vim ~/.vimrc"
+
+export PATH="$PATH:/Users/ono/.bin"
